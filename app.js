8d159698810c8d3fc19f250efe545e1f058dce78
Jeff Titak <jtitak06@yahoo.com>
2021-12-02 17:56:56 +0300

    Completed assignment

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 
 const http = require("http");
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("Hello World!");
+})
+.listen(8080, function () {
+    console.log("Server listening...")
+})
 
 // Finish setting up the server
